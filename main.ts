5e7fb008a8b943477d6dec93e958a50a2a7674ca
saniya kanwal <kashan0135@gmail.com>
2024-04-05 09:35:10 -0700

    ATM

---
+++ main.ts
@@ -0,0 +1,61 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 40000; //Dollar
+let pinCode = 3456;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "enter your pin",
+            type: "number",
+        }
+    ] 
+);
+
+    if (pinAnswer.pin === pinCode) {
+    console.log("Correct pin code !!!")
+
+    let operationAns = await inquirer.prompt(
+        [
+           {
+              name:"operation",
+              message:"please select option",
+              type:"list",
+              choices:["Check Balance","Withdraw money",""]
+           }
+        ]
+    );
+    
+    console.log(operationAns);
+    
+    //withdraw money
+    if (operationAns.operation === "Withdraw money") {
+        let  amountAns = await inquirer.prompt(
+            [    
+                {
+                   name:"amount",
+                   message: "how much do you want to withdraw?",
+                   type:"number"
+                }
+            ]
+    );
+
+  //withdraw answers
+    if(amountAns.amount <= myBalance){
+    myBalance -= amountAns.amount
+          console.log(`your remaining balance is: ${myBalance}`)
+    }else{
+        console.log("insufficient balance");
+    }
+    }
+    //check balance
+if(operationAns.operation === "Check Balance"){
+        console.log(`your balance is: ${myBalance}`)
+    }
+ }
+
+ else{
+    console.log("Incorrect pin code");
+ }
